6b9db70a81c85a9ac3dc7d0e159082b5cec908d9
Vlad <litlelegazp@gmail.com>
2025-03-12 21:45:21 +0200

    init

--- dw.py
+++ dw.py
@@ -1,1 +1,15 @@
-print("vmenelazerizochei")
+print("Яка там в тебе оцінка?")
+otsenka = int(input())
+
+if 1 <= otsenka < 6:
+    print("Нє ну ти канєшна капееець")
+
+elif 6 <= otsenka < 9:
+    print("Нє ну норм")
+
+elif 9 <= otsenka <= 12:
+    print("Оце вже нічо сє")
+
+
+
+
